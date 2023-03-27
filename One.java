ce75a5f649efbce76e52c8f217fbe418f0b27c01
hereazim <106958262+hereazim@users.noreply.github.com>
2023-03-27 12:51:19 +0530

    line removed

--- One.java
+++ One.java
@@ -1,6 +1,5 @@
 public class One{
   public static void main(String[] args){
    System.out.println("This is an example for Git and Github...");
-    System.out.println("This line is added!");
   }
 }
